19e04512363c37d90fdb24f71e90a1896f8871aa
github_username <you@domain.com>
2023-06-07 13:08:32 +0600

    gellary section added

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -5,10 +5,26 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
 import { FreeMode, Pagination } from "swiper";
+import category2 from '../../assets/category/swipe-2.avif'
+import category1 from '../../assets/category/swipe1.avif'
+import category3 from '../../assets/category/swipe3.avif'
+import category4 from '../../assets/category/swipe4.avif'
+import category5 from '../../assets/category/swipe5.avif'
+import category6 from '../../assets/category/swipe6.avif'
+import category7 from '../../assets/category/swipe7.avif'
+import category8 from '../../assets/category/swipe8.avif'
+import category9 from '../../assets/category/swipe9.avif'
+import SectionTitle from "../Shared/SectionTitle/SectionTitle";
+
 
 const Category = () => {
     return (
         <>
+        <SectionTitle
+        heading='Gellary'
+        subHeading='Dispaly'
+
+        ></SectionTitle>
               <Swiper
         slidesPerView={3}
         spaceBetween={30}
@@ -17,17 +33,17 @@ const Category = () => {
           clickable: true,
         }}
         modules={[FreeMode, Pagination]}
-        className="mySwiper"
+        className="mySwiper my-10"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide><img className="h-60"  src={category1} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="h-60" src={category2} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="h-60" src={category3} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="h-60" src={category4} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="h-60" src={category5} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="h-60" src={category6} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="h-60" src={category7} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="h-60" src={category8} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="h-60" src={category9} alt="" /></SwiperSlide>
       </Swiper>
         </>
     );
